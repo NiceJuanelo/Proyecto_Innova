29ae8767c77484daae82a6e2adedd74a9211f3b3
Saul <saulb3564@gmail.com>
2023-04-04 19:50:20 -0600

    se agrego logo e imagenes de bienvenida.

---
+++ src/components/BienvenidaCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-card
+    flat
+    class="card"
+  >
+    <q-card-section
+      class="q-pa-none section bg-black"
+      vertical
+    >
+      <q-img
+        :src="image1"
+        style="width: 50%; height: calc((100vh - 106px) / 2)"
+      />
+      <q-img
+        :src="image2"
+        style="width: 50%; height: calc((100vh - 106px) / 2)"
+      />
+    </q-card-section>
+    <q-img
+      class="img-logo"
+      :src="logoApp"
+      width="400px"
+    />
+  </q-card>
+</template>
+
+<style scoped>
+.card {
+  height: calc(100vh - 106px);
+  position: relative;
+  z-index: 5;
+}
+.section {
+  display: flex;
+  flex-direction: column;
+  z-index: 6;
+}
+
+.img-logo {
+  position: absolute;
+  z-index: 10;
+  top: calc(25vh / 2);
+  right: calc(12.5% / 2);
+}
+</style>
+
+<script setup>
+import image1 from '@/assets/fondo-inicio-1.svg';
+import image2 from '@/assets/fondo-inicio-2.svg';
+import logoApp from '@/assets/logo-workersapp.svg';
+</script>
--- src/views/WelcomeCard.vue
+++ src/views/WelcomeCard.vue
@@ -1,6 +1,10 @@
 <template>
   <NavBar />
+  <q-page>
+    <BienvenidaCard />
+  </q-page>
 </template>
 <script setup>
 import NavBar from '../components/NavBar.vue';
+import BienvenidaCard from '../components/BienvenidaCard.vue';
 </script>
