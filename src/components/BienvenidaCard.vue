3a871ac65b01211504884e6ee8a976c337d3e192
saul <saulb3564@gmail.com>
2023-04-07 01:52:52 -0600

    Se termino la pantalla de bienvenida.

--- src/components/BienvenidaCard.vue
+++ src/components/BienvenidaCard.vue
@@ -9,17 +9,28 @@
     >
       <q-img
         :src="image1"
-        style="width: 50%; height: calc((100vh - 106px) / 2)"
+        style="width: 56.5%; height: calc((100vh - 106px) / 2)"
       />
       <q-img
         :src="image2"
-        style="width: 50%; height: calc((100vh - 106px) / 2)"
+        style="width: 56.5%; height: calc((100vh - 106px) / 2)"
       />
     </q-card-section>
-    <q-img
-      class="img-logo"
-      :src="logoApp"
-    />
+    <div class="container-imgs">
+      <div class="container-img_cont">
+        <q-img
+          class="img-logo"
+          :src="logoApp"
+        />
+        <div class="canvas-relleno">
+          <div class="canvas"></div>
+        </div>
+        <div class="canvas1-relleno">
+          <div class="canvas1"></div>
+        </div>
+        <div class="canvas2"></div> 
+      </div>
+    </div>
   </q-card>
 </template>
 
@@ -41,7 +52,73 @@
   z-index: 10;
   top: calc(100vh / 6.5);
   width: calc(100vh / 1.8);
-  right: calc(25% / 2.5);
+  right: calc(25% / 1.5);
+}
+
+.container-imgs {
+  z-index: 7;
+  height: calc(100vh - 106px);
+  width: calc(100% / 1.26);
+  right: 0;
+  top: 0;
+  position: absolute;
+}
+
+.container-img_cont {
+  width: 100;
+  height: 100%;
+  position: relative;
+}
+
+.canvas {
+  z-index: 7;
+  width: 0;
+  height: 0;
+  right: 100%;
+  position: absolute;
+  border-right: 400px solid rgba(240, 173, 78, 0.53);
+  border-top: calc((100vh - 106px) / 2) solid transparent;
+  border-bottom: calc((100vh - 106px) / 2) solid transparent;
+}
+
+.canvas-relleno {
+  z-index: 6;
+  position: absolute;
+  width: 87.8px;
+  right: calc(100% / 1.819);
+  height: 100%;
+  background-color: rgba(240, 173, 78, 0.53);
+}
+
+.canvas1 {
+  z-index: 8;
+  width: 0;
+  height: 0;
+  position: absolute;
+  right: 100%;
+  border-right: 400px solid #F47C0C;
+  border-top: calc((100vh - 106px) / 2) solid transparent;
+  border-bottom: calc((100vh - 106px) / 2) solid transparent;
+}
+
+.canvas1-relleno {
+  z-index: 7;
+  position: absolute;
+  width: 95px;
+  right: calc(100% / 2.166);
+  height: 100%;
+  background-color: #F47C0C;
+}
+
+.canvas2 {
+  z-index: 9;
+  width: 0;
+  height: 0;
+  position: absolute;
+  right: 46%;
+  border-right: 400px solid black;
+  border-top: calc((100vh - 106px) / 2) solid transparent;
+  border-bottom: calc((100vh - 106px) / 2) solid transparent;
 }
 </style>
 
