bb13d73d433d5cbb01c4d411fa17c5822645f2d1
Juan Antonio <tecnm.19640045@gmail.com>
2023-04-09 22:28:04 -0600

    Redireccionamiento al login

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-import App from "./App.vue";
-import router from "./router";
+import { Quasar } from 'quasar';
 
-import "./assets/main.css";
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
+import quasarUserOptions from './quasar-user-options';
+const app = createApp(App);
 
-const app = createApp(App).use(Quasar, quasarUserOptions);
+app.use(Quasar, quasarUserOptions);
 
 app.use(createPinia());
 app.use(router);
-
-app.mount("#app");
+app.mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,16 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      redirect: "/login",
+      path: '/',
+      redirect: '/login',
     },
     {
-      path: "/",
-      name: "login",
-      component: () => import("../components/Login.vue"),
-    },
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
     },
   ],
 });
