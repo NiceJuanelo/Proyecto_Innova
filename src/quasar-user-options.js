d50c98b27efa52ba6b06d6f8bba72b2515bcb478
saul <saulb3564@gmail.com>
2023-04-13 18:41:51 -0600

    Se agrego notify a los plugins

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -1,10 +1,12 @@
 import 'quasar/dist/quasar.css';
 import lang from 'quasar/lang/es.js';
 import '@quasar/extras/material-icons/material-icons.css';
-
+import { Notify } from 'quasar';
 // To be used on app.use(Quasar, { ... })
 export default {
   config: {},
-  plugins: {},
+  plugins: {
+    Notify
+  },
   lang: lang,
 };
