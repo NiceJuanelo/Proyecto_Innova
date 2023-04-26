7b000cd6aa8c82f2aa0df4e26aa2d5e08a68b2d3
saul <saulb3564@gmail.com>
2023-04-26 00:55:46 -0600

    Se agrego pantalla principal del cliente

---
+++ src/views/HomeClient.vue
@@ -0,0 +1,11 @@
+<template>
+  <NavBarHome />
+  <q-page>
+    <CardHomeClient />
+  </q-page>
+</template>
+
+<script setup>
+import CardHomeClient from '@/components/CardHomeClient.vue';
+import NavBarHome from '../components/NavBarHome.vue';
+</script>
