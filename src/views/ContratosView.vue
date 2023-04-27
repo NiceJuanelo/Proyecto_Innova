6b992c05534ae702ce59491546eb9e80a4dc9f6a
saul <saulb3564@gmail.com>
2023-04-27 02:02:11 -0600

    Se agrego la pantalla de los contratos del usuario

---
+++ src/views/ContratosView.vue
@@ -0,0 +1,11 @@
+<template>
+  <NavBarHome />
+  <q-page padding>
+    <ContratosCard />
+  </q-page>
+</template>
+
+<script setup>
+import NavBarHome from '../components/NavBarHome.vue';
+import ContratosCard from '../components/ContratosCard.vue';
+</script>
