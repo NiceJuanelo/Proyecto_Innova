8e438b22dce8543b7ef234b549aeb4d47fc920ec
saul <saulb3564@gmail.com>
2023-04-27 01:07:30 -0600

    Se agrego la vista para las compras del cliente

---
+++ src/views/ComprasClientView.vue
@@ -0,0 +1,9 @@
+<template>
+  <NavBarHome />
+  <ComprasCard />
+</template>
+
+<script setup>
+import NavBarHome from '../components/NavBarHome.vue';
+import ComprasCard from '../components/ComprasCard.vue';
+</script>
